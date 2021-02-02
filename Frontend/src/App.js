2f5f5bdfadb87f0e78cf9b2727c791dcab31cc0b
aleegs <aleej.gs@gmail.com>
2021-02-02 18:20:55 -0300

    Initial commit (home template)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import { DatePicker } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
+import Home from './components/home/index'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Holanda</h1>
-      <DatePicker/>
-    </div>
+      <Home />
   );
 }
 
