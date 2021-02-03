38be5e25d007970d9f4d680031ec0dd9c6735a2f
aleegs <aleej.gs@gmail.com>
2021-02-03 20:10:14 -0300

    Reorganización

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
 
-function App() {
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
+
+/* Use AnimationRevealPage as a wrapper component for your pages if you are custom building it */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+import Homepage from "test/Homepage.js"
+import Register from "test/Register.js"
+import Login from "test/Login.js"
+import PrivacyPolicy from "test/PrivacyPolicy.js"
+import TermsOfService from "test/TermsOfService.js"
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  // return <AnimationRevealPage disabled></AnimationRevealPage>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/components/:type/:subtype/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/components/:type/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/treact">
+          <MainLandingPage />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/terms-of-service">
+          <TermsOfService />
+        </Route>
+        <Route path="/privacy-policy">
+          <PrivacyPolicy />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
